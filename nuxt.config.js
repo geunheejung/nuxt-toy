3668fa73682d907e84b0e221071a2757afca2ff7
geunheejung <geunhee0212@gmail.com>
2023-09-13 12:26:58 +0900

    css init.

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,10 +15,14 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: ['./assets/reset.css', './assets/main.scss'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: [
+    {
+      src: '@/plugins/register-component.ts',
+    },
+  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,

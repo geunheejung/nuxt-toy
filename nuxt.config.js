0e9a6c1479a919f92dd3b928b86d8c29f53fc2be
geunheejung <geunhee0212@gmail.com>
2023-09-13 13:08:52 +0900

    scss import 관련 라이브러리 추가.

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,11 +11,21 @@ export default {
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css',
+        integrity:
+          'sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==',
+        crossorigin: 'anonymous',
+        referrerpolicy: 'no-referrer"',
+      },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ['./assets/reset.css', './assets/main.scss'],
+  css: ['@/assets/styles/_reset.css', '@/assets/styles/_main.scss'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
@@ -32,7 +42,11 @@ export default {
     // https://go.nuxtjs.dev/typescript
     '@nuxt/typescript-build',
     'nuxt-typed-vuex',
+    '@nuxtjs/style-resources',
   ],
+  styleResources: {
+    scss: ['@/assets/styles/_main.scss'],
+  },
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [],
